cd5225586fa83883a5c3050c336f9bbf99d01c0c
mtfasmatharik-boop <mtfasmatharik@gmail.com>
2025-11-08 22:05:28 +0530

    first commit

---
+++ frontend/js/guider-script.js
@@ -0,0 +1,8 @@
+function nextStep() {
+    document.getElementById("step1").style.display = "none";
+    document.getElementById("step2").style.display = "block";
+  }
+  function prevStep() {
+    document.getElementById("step2").style.display = "none";
+    document.getElementById("step1").style.display = "block";
+  }
