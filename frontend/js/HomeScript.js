7fe39c1e1133a0e344a9d2765018f208bbb2137f
Dasun53 <dasununimail@gmail.com>
2025-11-09 23:21:52 +0530

    Adding Trip planning form

--- frontend/js/HomeScript.js
+++ frontend/js/HomeScript.js
@@ -20,3 +20,27 @@ for (let i = 0; i < animation_element.length; i++) {
 
   observer.observe(el);
 }
+
+// --------- Trip planning Form -------------
+
+// Get elements
+const openBtn = document.getElementById("openBtn");
+const modal = document.getElementById("popupForm");
+const closeBtn = document.querySelector(".close");
+
+// Open popup
+openBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+// Close popup when clicking "x"
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close popup when clicking outside the form
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
