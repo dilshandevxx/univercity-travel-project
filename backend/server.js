c428aabe55e93e8678f14051ef6a6fd503d8e4a3
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2025-11-12 22:45:37 +0530

    add dependansies

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,12 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../", "frontend")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path);
+  res.sendFile(
+    path.join(__dirname, "../", "frontend", "pages", "start-page.html")
+  );
 });
 
 const PORT = 5000;
