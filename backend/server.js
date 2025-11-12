b23fc6bac8cd2c9b5208da22138058ccd54305c8
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2025-11-12 18:47:56 +0530

    add route for start page

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend server running ");
+  res.sendFile(path);
 });
 
 const PORT = 5000;
